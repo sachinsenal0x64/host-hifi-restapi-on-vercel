c97938d3e53a454a42724f1c38ef3fa884412f9b
sachin senal <127573781+sachinsenal0x64@users.noreply.github.com>
2024-02-13 22:56:00 +0100

    Delete test_index.py

--- test_index.py
+++
@@ -1,9 +0,0 @@
-import pytest
-import json
-
-from src.index import root, get_track
-
-@pytest.mark.asyncio
-async def test_get_track():
-    result = await get_track()
-    assert result == result.json()
