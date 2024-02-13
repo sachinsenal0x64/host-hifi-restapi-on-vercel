31e10607592d5a00ec3baec06a4b099c70ca9813
sachin senal <127573781+sachinsenal0x64@users.noreply.github.com>
2024-02-13 22:53:55 +0100

    Update test_index.py

--- test_index.py
+++ test_index.py
@@ -3,6 +3,6 @@ import pytest
 from src.index import root, get_track
 
 @pytest.mark.asyncio
-async def get_track():
+async def test_get_track():
     result = await get_track()
     assert result == result
