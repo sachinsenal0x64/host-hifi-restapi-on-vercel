179deb496c1854f6caa70cb0a7d4d27b3077913a
sachin senal <127573781+sachinsenal0x64@users.noreply.github.com>
2024-02-13 22:54:25 +0100

    Update test_index.py

--- test_index.py
+++ test_index.py
@@ -1,8 +1,9 @@
 import pytest
+import json
 
 from src.index import root, get_track
 
 @pytest.mark.asyncio
 async def test_get_track():
     result = await get_track()
-    assert result == result
+    assert result == result.json()
